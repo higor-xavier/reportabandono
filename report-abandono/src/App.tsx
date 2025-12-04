5b95aa051e0d151af86403ac1f537eb9119d18cf
higor-xavier <higorsxavier@gmail.com>
2025-12-03 23:28:52 -0300

    Instalação do shadcn-UI

---
+++ report-abandono/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button variant="outline">Enviar</Button>
+    </div>
+  )
+}
